9f08179086d950021106adc465c7045b77e3c7ab
gerrardmourao <116206155+gerrardmourao@users.noreply.github.com>
2023-09-26 22:59:57 -0300

    exercicios 2

--- listaDeExercícios2.sql
+++ listaDeExercícios2.sql
@@ -12,3 +12,22 @@ END;
 DELIMITER ;
 CALL sp_ListarAutores();
 
+
+-- EX 2:
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(200))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID WHERE Categoria.Nome = categoria_nome
+END;
+//
+
+DELIMITER ;
+
+
+CALL sp_LivrosPorCategoria('Romance');
+CALL sp_LivrosPorCategoria('Ciência');
+CALL sp_LivrosPorCategoria('Ficção Científica');
+CALL sp_LivrosPorCategoria('História');
+CALL sp_LivrosPorCategoria('Autoajuda');
